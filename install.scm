53984975e6ae5b442a2ea8cc74b5b2c725eaa4b8
mytoh <mytoh@gmail.com>
2012-06-17 05:15:18 +0900

    edit install script

--- install.scm
+++ install.scm
@@ -14,11 +14,12 @@
     (print "coping library directory")
     (copy-directory*
       "kirjasto"
-      *panna-directory*)))
+      (build-path *panna-directory*
+                  "kirjasto"))))
 
 (define (main args)
 (cond
-  ( (file-exists? (build-path (home-directory) ".panna"))  
+  ((file-exists? (build-path (home-directory) ".panna"))  
    (print "please remove ~/.panna directory"))
   (else
     (install)))) 
