97f8c92a734e440882b1aceae84c12fe9e567bdf
mytoh <mytoh@gmail.com>
2012-08-04 14:02:28 +0900

    update

--- kirjasto/kaava/vim.scm
+++ kirjasto/kaava/vim.scm
@@ -5,25 +5,48 @@
 (repository "https://vim.googlecode.com/hg/")
 
 (define (install tynnyri)
-  (with-clang)
-  (system
-    `(./configure   ,(string-append
-                       "--prefix=" tynnyri)
-                    "--cache-file=/dev/null"
-                    "--with-features=huge"
-                    "--enable-multibyte"
-                    "--enable-perlinterp"
-                    "--enable-pythoninterp"
-                    ; "--enable-mzschemeinterp"
-                    "--enable-xim"
-                    "--enable-fontset"
-                    "--disable-darwin"
-                    "--disable-selinux"
-                    "--with-x"
-                    "--with-features=huge")
-    '(gmake)
-    '(gmake install)
-    '(gmake clean)
-    '(gmake distclean)
-    ))
+  (cond 
+    ((is-freebsd)
+     (with-clang)
+     (system
+       `(./configure   ,(string-append
+                          "--prefix=" tynnyri)
+                       "--cache-file=/dev/null"
+                       "--with-features=huge"
+                       "--enable-multibyte"
+                       "--enable-perlinterp"
+                       "--enable-pythoninterp"
+                       ; "--enable-mzschemeinterp"
+                       "--enable-xim"
+                       "--enable-fontset"
+                       "--disable-darwin"
+                       "--disable-selinux"
+                       "--with-x"
+                       "--with-features=huge")
+       '(gmake)
+       '(gmake install)
+       '(gmake clean)
+       '(gmake distclean)))
+    (else
+     (system
+       `(./configure   ,(string-append
+                          "--prefix=" tynnyri)
+                       "--cache-file=/dev/null"
+                       "--with-features=huge"
+                       "--enable-multibyte"
+                       "--enable-perlinterp"
+                       "--enable-pythoninterp"
+                       ; "--enable-mzschemeinterp"
+                       "--enable-xim"
+                       "--enable-fontset"
+                       "--disable-darwin"
+                       "--disable-selinux"
+                       "--with-x"
+                       "--with-features=huge")
+       '(make)
+       '(make install)
+       '(make clean)
+       '(make distclean)) 
+      ))
+  )
 
