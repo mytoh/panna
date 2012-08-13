dda33f6c34406e6ef5f74ee579cf54ba6ea57f01
mytoh <mytoh@gmail.com>
2012-08-13 23:51:26 +0900

    update

--- kirjasto/kaava/tmux.scm
+++ kirjasto/kaava/tmux.scm
@@ -4,18 +4,28 @@
 
 (kaava "tmux")
 (homepage "tmux.sourceforge.net")
-(repository "https://tmux.svn.sourceforge.net/svnroot/tmux")
+(repository "https://tmux.svn.sourceforge.net/svnroot/tmux/trunk")
 
 (define (install tynnyri)
-  (make-directory* "etc" )
-  (system
-    ; '(make clean)
-    ; '(make distclean)
-    '(aclocal)
-    '(automake --add-missing --force-missing --copy --foreign)
-    '(autoreconf)
-    `(./configure ,(string-append "--prefix=" tynnyri)
-                  ,(string-append "--exec-prefix=" tynnyri))
-    '(make)
-    '(make install)
-    ))
+  (cond
+    ((is-freebsd)
+     (system
+       '(aclocal)
+       '(automake --add-missing --force-missing --copy --foreign)
+       '(autoreconf)
+       `(./configure ,(string-append "--prefix=" tynnyri)
+                     ,(string-append "--exec-prefix=" tynnyri))
+       '(make)
+       '(make install)
+       '(make clean)
+       '(make distclean)))
+    (else
+      (system
+        '(sh ./autogen.sh)
+        `(./configure ,(string-append "--prefix=" tynnyri)
+                      ,(string-append "--exec-prefix=" tynnyri))
+        '(make)
+        '(make install)
+        '(make clean)
+        '(make distclean))
+      )))
