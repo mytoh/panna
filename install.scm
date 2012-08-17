0a624eef15dcd501c0beed0221ff38a9291bde3b
mytoh <mytoh@gmail.com>
2012-08-17 15:17:51 +0900

    update

--- install.scm
+++ install.scm
@@ -2,48 +2,53 @@
 (use gauche.process)
 
 (define *panna-directory*
-  (build-path (home-directory)
-              ".panna"))
-
-
+  (if (sys-getenv "OLUTPANIMO")
+    (sys-getenv "OLUTPANIMO")
+    (build-path (home-directory)
+                ".panna")))
 
 (define install
   (lambda ()
-    (print "creating panna directory")
-    (make-directory*
-      *panna-directory*)
+    (run-process `(git clone git://github.com/mytoh/panna ,*panna-directory*) :wait #t)))
 
-    (print "copying library directory")
-    (copy-directory*
-      "kirjasto"
-      (build-path *panna-directory*
-                  "kirjasto"))
 
-    (print "creating bin/ directory")
-    (make-directory*
-      (build-path *panna-directory*
-                  "bin"))
-
-   (print "creating riisi directory") 
-   (make-directory*
-     (build-path *panna-directory*
-                 "riisi"))
-
-    (print "linking run script")  
-    (sys-symlink
-      (build-path *panna-directory*
-                  "kirjasto/run-panna.scm")
-      (build-path *panna-directory*
-                  "bin/panna")) 
-
-    (print "install executable")  
-    (sys-rename
-      (build-path *panna-directory*
-                  "kirjasto/run-panna.scm")
-      (build-path *panna-directory*
-                  "bin/panna"))
-    (run-process `(chmod +x ,(build-path *panna-directory* "bin/panna")) :wait #t)
-    ))
+; (define install
+;   (lambda ()
+;     (print "creating panna directory")
+;     (make-directory*
+;       *panna-directory*)
+; 
+;     (print "copying library directory")
+;     (copy-directory*
+;       "kirjasto"
+;       (build-path *panna-directory*
+;                   "kirjasto"))
+; 
+;     (print "creating bin/ directory")
+;     (make-directory*
+;       (build-path *panna-directory*
+;                   "bin"))
+; 
+;     (print "creating riisi directory") 
+;     (make-directory*
+;       (build-path *panna-directory*
+;                   "riisi"))
+; 
+;     (print "linking run script")  
+;     (sys-symlink
+;       (build-path *panna-directory*
+;                   "kirjasto/run-panna.scm")
+;       (build-path *panna-directory*
+;                   "bin/panna")) 
+; 
+;     (print "install executable")  
+;     (sys-rename
+;       (build-path *panna-directory*
+;                   "kirjasto/run-panna.scm")
+;       (build-path *panna-directory*
+;                   "bin/panna"))
+;     (run-process `(chmod +x ,(build-path *panna-directory* "bin/panna")) :wait #t)
+;     ))
 
 (define (main args)
   (cond
