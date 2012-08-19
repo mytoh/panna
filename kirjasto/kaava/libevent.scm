dbbebad0ee50792d7e44514b36008122ec205dc6
mytoh <mytoh@gmail.com>
2012-08-19 19:04:03 +0900

    add libevent file

---
+++ kirjasto/kaava/libevent.scm
@@ -0,0 +1,19 @@
+;;; -*- coding: utf-8 -*-
+
+(use panna.kaava)
+
+(kaava "libevent")
+(homepage "http://libevent.org")
+(repository "git://github.com/libevent/libevent")
+
+(define (install tynnyri)
+  (with-clang)
+  (system
+    `(./configure ,(string-append "--prefix=" tynnyri))
+    '(make)
+    '(make verify)
+    '(make install)
+    '(make clean)
+    '(make distclean)
+    ))
+
