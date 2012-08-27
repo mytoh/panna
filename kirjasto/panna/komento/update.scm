de38746bc2abb0c615083c36b761753ffff6f636
mytoh <mytoh@gmail.com>
2012-08-27 23:09:12 +0900

    update: change functions

--- kirjasto/panna/komento/update.scm
+++ kirjasto/panna/komento/update.scm
@@ -7,113 +7,93 @@
 
 (use panna)
 
+
+(define (vcs-directory dir)
+  (cond
+    ((file-exists? (build-path dir ".hg"))
+     'hg)
+    ((file-exists? (build-path dir ".git"))
+     'git)
+    ((file-exists? (build-path dir ".svn"))
+     'svn)
+    ((file-exists? (build-path dir "CVS"))
+     'cvs)
+    ((file-exists? (build-path dir ".bzr"))
+     'bzr)))
+
+(define (git-update dir)
+  (let ((pout (process-output->string '(git pull --rebase))))
+    (cond
+      ((string=? pout "Current branch master is up to date.")
+       (sys-select #f #f #f 100000)
+       (display "\r")
+       (run-process '(tput el1) :wait #t)
+       (message "none to update ")
+       (display (colour-string (colour-package) (last (string-split dir "/"))))
+       (newline)
+       (flush)
+       (sys-select #f #f #f 100000))
+      (else
+        (newline)
+        (message "Updated ")
+        (display (colour-string 194 (last (string-split dir "/"))))
+        (newline)
+        (newline)
+        (print pout)))))
+
+
+(define (update-repo dir)
+  (current-directory dir)
+  (display (colour-string (colour-symbol1) ">> "))
+  (display (colour-string 114 "Updating "))
+  (display (colour-string (colour-package) (last (string-split dir "/"))))
+  (flush)
+  (ecase (vcs-directory dir)
+    ((hg)
+     (commands
+       '(hg pull)
+       '(hg update)))
+    ((git)
+     (git-update dir))
+    ((svn)
+     (commands
+       '(svn update)))
+    ((bzr)
+     (commands
+       '(bzr update)))
+    ((cvs)
+     (commands
+       '(cvs update)))))
+
+(define (update-panna)
+  (current-directory (panna-kansio))
+  (message  "updating panna repository")
+  (newline)
+  (flush)
+  (commands '(git pull)))
+
 (define (update . pullo)
-  (let* ((vcs-directory
-           (lambda (dir)
-             (cond
-               ((file-exists? (build-path dir ".hg"))
-                'hg)
-               ((file-exists? (build-path dir ".git"))
-                'git)
-               ((file-exists? (build-path dir ".svn"))
-                'svn)
-               ((file-exists? (build-path dir "CVS"))
-                'cvs)
-               ((file-exists? (build-path dir ".bzr"))
-                'bzr))))
-         (git-update (lambda (repo)
-                       (let ((pout (process-output->string '(git pull --rebase))))
-                         (cond
-                           ((string=? pout "Current branch master is up to date.")
-                            (sys-select #f #f #f 100000)
-                            (display "\r")
-                            (run-process '(tput el1) :wait #t)
-                            (message "none to update ")
-                            (display (colour-string (colour-package) (last (string-split repo "/"))))
-                            (newline)
-                            (flush)
-                            (sys-select #f #f #f 100000))
-                           (else
-                             (newline)
-                             (message "Updated ")
-                             (display (colour-string 194 (last (string-split repo "/"))))
-                             (newline)
-                             (print pout)))))))
     (cond
       ((not (null-list? pullo))
        ; update one repository
        (let ((riisi (build-path (riisi-kansio) (car pullo))))
          ; update panna
-         (current-directory (panna-kansio))
-         (message  "updating panna repository")
-         (newline)
-         (commands '(git pull))
-
-         (current-directory riisi)
-         (message  "updating ")
-         (display (colour-string (colour-package) (last (string-split (current-directory) "/"))))
-         (newline)
-         (ecase (vcs-directory riisi)
-           ((hg)
-            (commands
-              '(hg pull)
-              '(hg update)))
-           ((git)
-            (commands
-              '(git pull --rebase --all)))
-           ((svn)
-            (commands
-              '(svn update)))
-           ((bzr)
-            (commands
-              '(bzr update)))
-           ((cvs)
-            (commands
-              '(cvs update))))))
+         (update-panna)
+         (update-repo riisi)))
 
       ; update all repositories
       (else
         (let* ((repos  (directory-list (riisi-kansio) :children? #t :add-path? #t)))
           ; update panna
-          (current-directory (panna-kansio))
-          (message  "updating panna repository")
-          (newline)
-          (commands '(git pull))
-          (flush)
+          (update-panna)
           (for-each
             (lambda (repo)
-              (current-directory repo)
-
-              (display (colour-string (colour-symbol1) ">> "))
-              (display (colour-string 114 "Updating "))
-              (display (colour-string (colour-package) (last (string-split repo "/"))))
-              (flush)
-              ; (newline)
-
-              (ecase (vcs-directory (current-directory))
-                ((hg)
-                 (commands
-                   '(hg pull)
-                   '(hg update)))
-                ((git)
-                 (git-update repo)
-                 ; (commands
-                 ; '(git pull --rebase))
-                 )
-                ((svn)
-                 (commands
-                   '(svn update)))
-                ((cvs)
-                 (commands
-                   '(cvs update)))
-                ((bzr)
-                 (commands
-                   '(bzr update)))))
-
-            repos))))))
+              (update-repo repo))
+            repos)))))
 
 
 (define (main args)
+  (print "debuging")
   (match (length (cdr args))
     (1 (update (cadr args)))
     (0 (update))))
