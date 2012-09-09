dfcacc73c38b065e7fdf82cc76fca587b09ce28b
mytoh <mytoh@gmail.com>
2012-09-09 11:42:40 +0900

    update

--- install.scm
+++ install.scm
@@ -12,28 +12,17 @@
     (print "cloning repository")
     (run-process `(git clone git://github.com/mytoh/panna ,*panna-directory*) :wait #t)
 
-    (print "creating bin/ directory")
-    (make-directory*
-      (build-path *panna-directory*
-                  "bin"))
+    ; (print "creating bin/ directory")
+    ; (make-directory*
+    ;   (build-path *panna-directory*
+    ;               "bin"))
 
     (print "creating riisi directory")
     (make-directory*
       (build-path *panna-directory*
                   "riisi"))
-    (print "linking run script")
-    (sys-symlink
-      (build-path *panna-directory*
-                  "kirjasto/run-panna.scm")
-      (build-path *panna-directory*
-                  "bin/panna"))
 
-    (print "install executable")
-    (sys-rename
-      (build-path *panna-directory*
-                  "kirjasto/run-panna.scm")
-      (build-path *panna-directory*
-                  "bin/panna"))
+    (print "make executable")
     (run-process `(chmod +x ,(build-path *panna-directory* "bin/panna")) :wait #t)
 
     (newline)
