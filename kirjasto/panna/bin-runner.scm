ef21da66266d5b07011f1f16db04ed28535a0aa1
mytoh <mytoh@gmail.com>
2012-09-10 16:11:27 +0900

    change usage

--- kirjasto/panna/bin-runner.scm
+++ kirjasto/panna/bin-runner.scm
@@ -56,7 +56,7 @@
   (let-args (cdr args)
     ((search "S|search")
      (prefix "prefix" )
-     (#f "h|help" => usage)
+     (help "h|help")
      (else (opt . _) (print "Unknown option :" opt) (usage))
      . rest)
     (let* ((komento-directory
@@ -101,6 +101,7 @@
         (cond
           (search (panna "search"))
           (prefix (print (get-environment-variable "OLUTPANIMO")))
+          (help   (panna "help"))
           (else
             (match (car rest)
               ; command aliases
@@ -125,77 +126,4 @@
               (_ (panna (car rest)))))))))
   0)
 
-
-; (define (bin-runner args)
-;   (let-args (cdr args)
-;     ((search "S|search")
-;      (prefix "prefix" )
-;      (#f "h|help" => usage)
-;      (else (opt . _) (print "Unknown option :" opt) (usage))
-;      . rest)
-;     (if (null-list? (cdr args))
-;       (usage)
-;       (let* ((komento-directory
-;                (build-path (sys-getenv "OLUTPANIMO")
-;                            "kirjasto/panna/komento/"))
-;              (kaava (match (length rest)
-;                       (2 (cdr rest))
-;                       (1  (if search rest #f))
-;                       (0 #f)
-;                       (_ (cdr rest))))
-;              (panna (lambda (c)
-;                       (cond
-;                         (kaava
-;                           (if (file-exists? (build-path
-;                                               komento-directory
-;                                               (path-swap-extension c "scm")))
-;                             (run-process `(gosh ,(build-path
-;                                                    (sys-getenv "OLUTPANIMO")
-;                                                    "kirjasto/panna/komento/"
-;                                                    (path-swap-extension c "scm"))
-;                                                 ,@kaava)
-;                                          :wait #t)
-;                             (run-process `(gosh ,(build-path
-;                                                    (sys-getenv "OLUTPANIMO")
-;                                                    "kirjasto/panna/komento/install.scm")
-;                                                 ,@rest)
-;                                          :wait #t)))
-;                         (else
-;                           (if (file-exists? (build-path komento-directory
-;                                                         (path-swap-extension c "scm")))
-;                             (run-process `(gosh ,(build-path
-;                                                    komento-directory
-;                                                    (path-swap-extension c "scm")))
-;                                          :wait #t)
-;                             (run-process `(gosh ,(build-path
-;                                                    (sys-getenv "OLUTPANIMO")
-;                                                    "kirjasto/panna/komento/install.scm")
-;                                                 ,@rest)
-;                                          :wait #t)))))))
-;         (cond
-;           (search (panna "search"))
-;           (prefix (print (get-environment-variable "OLUTPANIMO")))
-;           (else
-;             (match (car rest)
-;               ; command aliases
-;               ((or "asentaa" "ase")
-;                (panna "install"))
-;               ("up"
-;                (panna "update"))
-;               ("ln"
-;                (panna "link"))
-;               ("ls"
-;                (panna "list"))
-;               ("home"
-;                (panna "homepage"))
-;               ((or "rm" "remove")
-;                (panna "uninstall"))
-;               ("abv"
-;                (panna "info"))
-;               ("env"
-;                (panna "environment"))
-;               ("help"
-;                (panna "help"))
-;               (_ (panna (car rest)))))))))
-;   0)
 
