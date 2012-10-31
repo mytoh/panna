e1f4355156f9ed763631213ce4dcba53ff9402ba
mytoh <mytoh@gmail.com>
2012-11-01 01:06:16 +0900

    update

--- kirjasto/lib/kaava/ypsilon.scm
+++ kirjasto/lib/kaava/ypsilon.scm
@@ -11,10 +11,10 @@
   (cond
     ((is-freebsd)
      (system
-       `(make ,(string-append "PREFIX=" tynnyri))
-       `(make ,(string-append "PREFIX=" tynnyri) install)
-       '(make clean)
-       '(make distclean)))
+       `(gmake ,(string-append "PREFIX=" tynnyri))
+       `(gmake ,(string-append "PREFIX=" tynnyri) install)
+       '(gmake clean)
+       '(gmake distclean)))
     (else
       (system
         `(make ,(string-append "PREFIX=" tynnyri))
