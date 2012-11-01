f0346d95c2e54c0107541ff58e09f26cc984ec18
mytoh <mytoh@gmail.com>
2012-11-01 13:43:35 +0900

    add sagittarius kaava

---
+++ kirjasto/lib/kaava/sagittarius.scm
@@ -0,0 +1,18 @@
+
+;;; -*- coding: utf-8 -*-
+
+(use panna.kaava)
+
+(kaava "sagittarius")
+(homepage "codes.google.com/p/sagittarius-scheme")
+(repository '("https://code.google.com/p/sagittarius-scheme" hg))
+
+(define (install tynnyri)
+  (make-directory* "build")
+  (current-directory "build")
+  (system
+    '(cmake ..)
+    '(make)
+    )
+  )
+
