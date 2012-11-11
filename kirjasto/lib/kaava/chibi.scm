51fb020966e3defe370b88e4b6937e3953600c80
mytoh <mytoh@gmail.com>
2012-11-11 23:01:32 +0900

    update

--- kirjasto/lib/kaava/chibi.scm
+++ kirjasto/lib/kaava/chibi.scm
@@ -7,13 +7,20 @@
 (repository '("https://code.google.com/p/chibi-scheme"  hg))
 
 (define (install tynnyri)
-  (with-clang)
-  (with-usr-local)
-  (system
-    '(gmake)
-    '(gmake test)
-    `( gmake install ,(string-append  "PREFIX=" tynnyri))
-    '(gmake clean)
-    '(gmake distclean)
-    ))
+  (cond
+    ((is-freebsd)
+     (with-usr-local)  
+     (system
+       '(gmake)
+       '(gmake test)
+       `(gmake install ,(string-append  "PREFIX=" tynnyri))
+       '(gmake clean)
+       '(gmake distclean)))
+    (else
+      (system
+        '(make)
+        '(make test)
+        `(make install ,(string-append  "PREFIX=" tynnyri))
+        '(make clean)
+        '(make distclean)))))
 
