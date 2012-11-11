204aad6907a29ea29145ad5be33cec597ea44af5
mytoh <mytoh@gmail.com>
2012-11-11 23:56:13 +0900

    update

--- kirjasto/lib/kaava/mosh.scm
+++ kirjasto/lib/kaava/mosh.scm
@@ -22,10 +22,9 @@
         '("./gen-git-build.sh")
         `("./configure" ,(string-append "--prefix=" prefix)
           ,(string-append "CPPFLAGS=-I/Users/" (sys-getenv "USER")
-                                              "/local/homebrew/include")
+                          "/local/homebrew/include")
           ,(string-append "LDFLAGS=-L/Users/" (sys-getenv "USER")
-                         "/local/homebrew/lib")
-          )
+                          "/local/homebrew/lib"))
         '(make)
         '(make install)))))
 
